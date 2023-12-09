2eb1d00b75f1f307febd2d8788f5c325ecf5c87c
Petr Novak <xskarou1@mail.muni.cz>
2023-12-09 10:54:07 +0100

    Add support for Moodle primal login page

--- scripts/content_moodle.js
+++ scripts/content_moodle.js
@@ -1,12 +1,20 @@
-userNameInput = document.getElementById('login_username');
-passwordInput = document.getElementById('login_password');
+userNameInputFirst = document.getElementById('login_username');
+userNameInputSecond = document.getElementById('username');
+userNameInput = userNameInputFirst ?? userNameInputSecond;
+
+passwordInputFirst = document.getElementById('login_password');
+passwordInputSecond = document.getElementById('password');
+passwordInput = passwordInputFirst ?? passwordInputSecond;
+
+submitButtonFirst = document.getElementsByClassName("btn btn-primary btn-block")[0];
+submitButtonSecond = document.getElementById('loginbtn');
+submitButton = submitButtonFirst ?? submitButtonSecond;
 
 if (userNameInput !== null && passwordInput !== null) {
 	chrome.storage.sync.get(['userName', 'password'], function(login) {
 		userNameInput.value = login.userName;
 		passwordInput.value = login.password;
 
-		submitButton = document.getElementsByClassName("btn btn-primary btn-block")[0];
 		submitButton.click();
     });
 }
